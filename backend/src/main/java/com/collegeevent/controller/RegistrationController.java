e2dc20b377d64948757fdcfeddbe3730c64febdf
vishnuvardhan43-tech <vishnuvardhan122505@gmail.com>
2026-07-22 18:02:38 +0530

    Complete Student Registration module

--- backend/src/main/java/com/collegeevent/controller/RegistrationController.java
+++ backend/src/main/java/com/collegeevent/controller/RegistrationController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/registration")
+@RequestMapping("/api/registrations")
 @CrossOrigin(origins = "http://localhost:5173")
 public class RegistrationController {
 
